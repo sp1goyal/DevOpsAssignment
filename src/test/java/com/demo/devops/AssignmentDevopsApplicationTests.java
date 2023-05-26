0e442866e5f0787753ff0919a4b6471a67698754
Sarthak Goyal <61136876+sp1goyal@users.noreply.github.com>
2023-05-26 13:46:05 +0530

    Update AssignmentDevopsApplicationTests.java

--- src/test/java/com/demo/devops/AssignmentDevopsApplicationTests.java
+++ src/test/java/com/demo/devops/AssignmentDevopsApplicationTests.java
@@ -1,6 +1,7 @@
 package com.demo.devops;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
