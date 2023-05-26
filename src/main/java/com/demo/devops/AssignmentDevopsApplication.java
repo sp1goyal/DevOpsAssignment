a6e5cb18ac276dd82575fa08d236df073eb56828
sp1goyal <sgoyalp1@gmail.com>
2023-05-26 09:42:54 +0530

    First Commit

---
+++ src/main/java/com/demo/devops/AssignmentDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.demo.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentDevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/devops/AssignmentDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
